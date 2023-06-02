7f090f082e7c24f3391d96d8c0327fb3c815fd96
meng <53615275+mengdeveloper@users.noreply.github.com>
2023-06-02 18:39:25 +0700

    Add files via upload

--- MicrosoftRBX-FPS/MicrosoftRBX-FPS/main.cpp
+++ MicrosoftRBX-FPS/MicrosoftRBX-FPS/main.cpp
@@ -4,26 +4,6 @@
 #include <vector>
 #include <thread>
 
-#define RST  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-
-#define FRED(x) KRED x RST
-#define FGRN(x) KGRN x RST
-#define FYEL(x) KYEL x RST
-#define FBLU(x) KBLU x RST
-#define FMAG(x) KMAG x RST
-#define FCYN(x) KCYN x RST
-#define FWHT(x) KWHT x RST
-
-#define BOLD(x) "\x1B[1m" x RST
-#define UNDL(x) "\x1B[4m" x RST
-
 HWND robloxHWND;
 bool isEnabled = false;
 
@@ -120,7 +100,6 @@ void fixCursor(HWND handle)
 void init()
 {
     // FindWindow is so fucked because it has none for this so i had to use this method <3
-    std::cout << KRED;
     std::cout << "Please open Microsoft Roblox Client and click somewhere.";
     while (true)
     {
@@ -132,7 +111,6 @@ void init()
             {
                 robloxHWND = currentHWND;
                 system("cls");
-                std::cout << KMAG;
                 std::cout << "Initialized!\nRoblox-Handle: " << currentHWND;
                 break;
             }
@@ -143,12 +121,9 @@ void init()
 
     Sleep(1000);
     system("cls");
-    std::cout << KBLU;
-    std::cout << BOLD("Roblox-HWND: ") << robloxHWND << std::endl;
-    std::cout << KYEL;
-    std::cout << BOLD("Keybind: ") << "INSERT" << std::endl;
-    std::cout << KCYN;
-    std::cout << BOLD("Enabled: ") << (isEnabled ? "Enabled" : "Disabled");
+    std::cout << ("Roblox-HWND: ") << robloxHWND << std::endl;
+    std::cout << ("Keybind: ") << "INSERT" << std::endl;
+    std::cout << ("Enabled: ") << (isEnabled ? "Enabled" : "Disabled");
 }
 
 void check()
@@ -172,12 +147,9 @@ void toggle()
         {
             isEnabled = not isEnabled;
             system("cls");
-            std::cout << KBLU;
-            std::cout << BOLD("Roblox-HWND: ") << robloxHWND << std::endl;
-            std::cout << KYEL;
-            std::cout << BOLD("Keybind: ") << "INSERT" << std::endl;
-            std::cout << KCYN;
-            std::cout << BOLD("Enabled: ") << (isEnabled ? "Enabled" : "Disabled");
+            std::cout << ("Roblox-HWND: ") << robloxHWND << std::endl;
+            std::cout << ("Keybind: ") << "INSERT" << std::endl;
+            std::cout << ("Enabled: ") << (isEnabled ? "Enabled" : "Disabled");
             Sleep(1000);
         }
         Sleep(25);
@@ -186,8 +158,8 @@ void toggle()
 
 int main()
 {
-    std::cout << KYEL << "1. Force Center Lock\n2. Lock Border\n\n";
-    std::cout << KCYN << "Input: ";
+    std::cout << "1. Force Center Lock\n2. Lock Border\n\n";
+    std::cout << "Input: ";
     std::cin >> mode;
 
     if (mode == 1 || mode == 2)
